883bfd65da19c8924b20418eab68eda4b9ef4aa3
amitsharma-prog <amitsharma18543@gmail.com>
2020-07-02 12:31:05 +0530

    Call To Action, Whatsapp, Email Button Put on Website 02-07-2020

--- include/attire_social.php
+++ include/attire_social.php
@@ -1,13 +1,13 @@
 <div class="attire-social">
 	<div class="social-icon">
 		<div class="attire-social-icon">
-			<a href="https://www.facebook.com/ajaygandhi6970" style="color: #000;"><i class="fab fa-facebook-f"></i></a>
+			<a href="https://www.facebook.com/ajaygandhi6970" style="color: #000;"><img src="include/img/Facebook.png"></a>
 		</div>
 		<div class="attire-social-icon">
-			<a href="https://www.instagram.com/ajay_gandhi/?hl=en" style="color: #000;"><i class="fab fa-instagram"></i></a>
+			<a href="https://www.instagram.com/ajay_gandhi/?hl=en" style="color: #000;"><img src="include/img/Instagram.png"></a>
 		</div>
 		<div class="attire-social-icon">
-			<a href="" style="color: #000;"><i class="fab fa-linkedin-in"></i></a>
+			<a href="" style="color: #000;"><img src="include/img/Linkein.png"></a>
 		</div>
 	</div>
 </div>
--- include/script_function.php
+++ include/script_function.php
@@ -22,7 +22,7 @@
 </script>
 
 <!--Start of Tawk.to Script-->
-<script type="text/javascript">
+<!-- <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -32,5 +32,28 @@ s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
-</script>
+</script> -->
 <!--End of Tawk.to Script-->
+
+<script type="text/javascript">
+    (function () {
+        var options = {
+            // facebook: "458744854485", // Facebook page ID
+            whatsapp: "+91 9868958363", // WhatsApp number
+            email: "gandhirrcc@gmail.com", // Email
+            sms: "+91 9868958363", // Sms phone number	
+            call: "+91 9868958363", // Call phone number
+            company_logo_url: "http://attiremensbespoke.com/images/logo.jpg", // URL of company logo (png, jpg, gif)
+            greeting_message: "Hi, How may we help you? Just send us a message to get help.", // Text of greeting message
+            call_to_action: "Message us", // Call to action
+            button_color: "#383838", // Color of button
+            position: "right", // Position may be 'right' or 'left'
+            order: "whatsapp,sms,call,email" // Order of buttons
+        };
+        var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+<!-- /WhatsHelp.io widget -->  
